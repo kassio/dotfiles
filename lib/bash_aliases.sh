e1e76938012e3fdafa7092c4245537c67ff20fb7
Kassio Borges <kassioborgesm@gmail.com>
2014-10-29 11:35:36 -0200

    Add the basics bash aliases that I need in servers.

---
+++ lib/bash_aliases.sh
@@ -0,0 +1,8 @@
+alias vi='vim'
+alias ..='cd ..'
+alias ls='ls --color=auto -th'
+alias ll='ls -l'
+alias la='ll -A'
+alias grep='grep -v grep | grep'
+alias psg='ps aux | grep'
+export GREP_OPTIONS="--color=auto"
