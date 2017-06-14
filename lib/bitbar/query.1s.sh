d7043816145eae1541b81c116e4fcad8ffbd8f32
Kassio Borges <kassioborgesm@gmail.com>
2017-06-14 18:16:40 +0100

    Add bitbar with a query monitor plugin

---
+++ lib/bitbar/query.1s.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# ${HOME}/.query example:
+# # Connection
+# user="root"
+# password="root"
+# host="127.0.0.1"
+# port="3306"
+# database="store"
+# MYSQL_URL="mysql://${user}:${password}@${host}:${port}/${database}"
+#
+# # Query
+# query="select count(*) USERS from users"
+
+source ${HOME}/.query
+
+if [ -n "${query}" ]
+then
+  result=$(/usr/local/bin/mysql \
+    --host="${host}" \
+    --user="${user}" \
+    --password="${password}" \
+    --database="${database}" \
+    -e "${query}" \
+    2>/dev/null)
+
+  echo ${result:-FAIL}
+  echo "---"
+  echo "MYSQL_URL: ${MYSQL_URL}"
+  echo "query: ${query}"
+fi
