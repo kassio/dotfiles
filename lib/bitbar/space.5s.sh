2b0f1a63187e2673d69c3c0984874dc464d22f1b
Kassio Borges <kassioborgesm@gmail.com>
2017-06-26 18:35:45 +0100

    Better bitbar info on mem and cpu

--- lib/bitbar/space.5s.sh
+++ lib/bitbar/space.5s.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-echo "CPU $(ps -A -o %cpu | awk '{s+=$1} END {print s "%"}') | size=12"
+mem="MEM $(ps -A -o %mem | awk '{s+=$1} END {print s "%"}')"
+cpu="CPU $(ps -A -o %cpu | awk '{s+=$1} END {print s "%"}')"
+
+echo "${mem} ${cpu} | size=12"
