9c9a679ebee3387ee3da71df13ed5614f62f65b0
Kassio Borges <kassioborgesm@gmail.com>
2019-04-26 13:33:52 -0500

    Better iex colors and infinity inspect and history

--- install/rcfiles/home/iex.exs
+++ install/rcfiles/home/iex.exs
@@ -1,27 +1,36 @@
-IEx.configure(history_size: 9999)
-Application.put_env(:elixir, :ansi_enabled, true)
+# Add `import_file("~/.iex.exs")` to the project `.iex.exs` to load these
+# configurations
+IEx.configure(history_size: -1)
+
+IEx.configure(inspect: [limit: :infinity])
 
 IEx.configure(
   colors: [
+    enabled: true,
     eval_result: [:cyan, :bright],
     eval_error: [[:red, :bright, "\n▶▶▶\n"]],
-    eval_info: [:yellow, :bright]
-  ],
+    eval_info: [:blue, :bright]
+  ]
+)
+
+IEx.configure(
   default_prompt:
     [
       # cursor ⇒ column 1
       "\e[G",
       :blue,
       "%prefix",
-      :yellow,
-      "|",
+      [:green, :bright],
+      "< ",
       :blue,
       "%counter",
       " ",
-      :yellow,
-      "▶",
+      [:green, :bright],
+      ">",
       :reset
     ]
     |> IO.ANSI.format()
     |> IO.chardata_to_string()
 )
+
+Application.put_env(:elixir, :ansi_enabled, true)
