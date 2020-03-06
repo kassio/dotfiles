37044fb9c4370e465df28f3ab47014babddee33f
Kassio Borges <kassioborgesm@gmail.com>
2020-03-06 17:56:15 +0000

    Add an easy way to toggle ActiveRecord logs on console

--- lib/ruby_console.rb
+++ lib/ruby_console.rb
@@ -1,3 +1,14 @@
 def silence_rails_warnings!
   ActiveSupport::Deprecation.silenced = true
 end
+
+def toggle_active_record_log!
+  current = ActiveRecord::Base.logger
+
+  if current != @__original_active_record_logger
+    ActiveRecord::Base.logger = @__original_active_record_logger
+    @__original_active_record_logger = current
+  end
+
+  ActiveRecord::Base.logger
+end
