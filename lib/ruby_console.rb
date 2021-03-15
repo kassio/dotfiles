ce87998a07884c68b51d81f6b6db971781a6022c
Kassio Borges <kassioborgesm@gmail.com>
2021-03-15 17:53:24 +0000

    Fix ruby console to not fail when gems are not available

--- lib/ruby_console.rb
+++ lib/ruby_console.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
-require 'interactive_editor'
-
-def local_reload
-  load File.expand_path("ruby_console/extensions.rb", __dir__)
-
-  local_ruby_console = ".ruby_console.local"
-  load local_ruby_console if File.exist?(local_ruby_console)
+def local_reload(local_helpers = '.ruby_console.local')
+  load File.expand_path('ruby_console/extensions.rb', __dir__)
+  load local_helpers if File.exist?(local_helpers)
 
   __reload if defined?(__reload)
+end; local_reload
+
+begin
+  require 'interactive_editor'
+rescue LoadError => e
+  puts e.message
 end
-local_reload
