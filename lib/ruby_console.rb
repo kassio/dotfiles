c275f919b2ebfcd9ef53fd9015f23f36f80d072f
Kassio Borges <kassioborgesm@gmail.com>
2021-05-19 12:05:12 +0100

    Remove gem interactive_editor

--- lib/ruby_console.rb
+++ lib/ruby_console.rb
@@ -1,14 +1,8 @@
 # frozen_string_literal: true
 
-def local_reload(local_helpers = '.ruby_console.local')
-  load File.expand_path('ruby_console/extensions.rb', __dir__)
+def local_reload(local_helpers = ".ruby_console.local")
+  load File.expand_path("ruby_console/extensions.rb", __dir__)
   load local_helpers if File.exist?(local_helpers)
 
   __reload if defined?(__reload)
 end; local_reload
-
-begin
-  require 'interactive_editor'
-rescue LoadError => e
-  puts e.message
-end
