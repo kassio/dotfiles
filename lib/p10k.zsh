5533bee255d8b785eedd40ef0d80ea0c14bcf620
Kassio Borges <kassioborgesm@gmail.com>
2021-11-05 18:47:24 +0000

    Remove asdf from prompt

--- lib/p10k.zsh
+++ lib/p10k.zsh
@@ -26,7 +26,6 @@
     status                  # exit code of the last command
     command_execution_time  # duration of the last command
     background_jobs         # presence of background jobs
-    asdf                    # asdf version manager (https://github.com/asdf-vm/asdf)
     newline
   )
 
