22dfa4f473920bf0e70ff221fa2ea3fa96252272
Kassio Borges <kassioborgesm@gmail.com>
2022-06-02 21:23:33 +0100

    nvim: update gopls configs

--- stow/nvim/.config/nvim/lua/plugins/lsp/customizations.lua
+++ stow/nvim/.config/nvim/lua/plugins/lsp/customizations.lua
@@ -27,14 +27,16 @@ return {
   },
 
   gopls = {
-    root_dir = util.root_pattern('.git', 'go.mod'),
     settings = {
       gopls = {
-        experimentalPostfixCompletions = true,
+        experimentalPostfixCompletions = true, -- experimental
         gofumpt = false,
+        usePlaceholders = true,
+        staticcheck = true, -- experimental
         codelenses = {
           generate = true,
           gc_details = true,
+          tide = true,
         },
         analyses = {
           nilness = true,
