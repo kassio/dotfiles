838e4afe3639c69f8827fb258cbcb0d71d7d2cd6
Kassio Borges <kassioborgesm@gmail.com>
2022-06-13 16:03:50 +0100

    nvim: avoid cmdwindow errors

--- stow/nvim/.config/nvim/lua/plugins/todo.lua
+++ stow/nvim/.config/nvim/lua/plugins/todo.lua
@@ -1,1 +1,8 @@
+-- HACK: #104 Invalid in command-line window
+local hl = require('todo-comments.highlight')
+local highlight_win = hl.highlight_win
+hl.highlight_win = function(win, force)
+  pcall(highlight_win, win, force)
+end
+
 require('todo-comments').setup()
