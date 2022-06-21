00da898f8373f3c1ba22b088f864002fc322f91b
Kassio Borges <kassioborgesm@gmail.com>
2022-06-22 00:34:12 +0100

    nvim: remove workarounds

--- stow/nvim/.config/nvim/lua/my/commands.lua
+++ stow/nvim/.config/nvim/lua/my/commands.lua
@@ -28,11 +28,3 @@ command('SessionSave', vim.my.sessions.save, { nargs = '?' })
 command('SessionLoad', vim.my.sessions.load, {})
 command('SessionDestroy', vim.my.sessions.destroy, {})
 command('SessionDestroyAll', vim.my.sessions.destroy_all, {})
-
--- Work aroud to quit neovim with wq and x
--- when a terminal is opened
-command('Z', 'w | qa', {})
-cabbrev('wqa', 'Z')
-cabbrev('xa', 'Z')
-
-cabbrev('e', 'e | normal zz')
