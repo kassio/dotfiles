3d490248221c301695f272a4c53e0628f2e24adf
Kassio Borges <kassioborgesm@gmail.com>
2022-06-23 21:38:37 +0100

    nvim: :nail_care:

--- stow/nvim/.config/nvim/lua/my/winbar.lua
+++ stow/nvim/.config/nvim/lua/my/winbar.lua
@@ -14,33 +14,36 @@ local winbar = function(focused)
   return function()
     if vim.tbl_contains(winbar_filetype_exclude, vim.bo.filetype) then
       vim.opt_local.winbar = ''
-    else
-      local hl_background = focused and 'WinBar' or 'WinBarNC'
-      local hl_filename = 'WinBarNC'
-      if focused then
-        hl_filename = 'WinBarInfo'
-      end
-      if vim.bo.modified then
-        hl_filename = 'WinBarWarn'
-      end
-
-      pcall(
-        vim.api.nvim_set_option_value,
-        'winbar',
-        table.concat({
-          string.format('%%#%s#', hl_filename),
-          ' %.100t',
-          string.format('%%#%s#', hl_background),
-          theme.icons.separator,
-          vim.my.treesitter.gps.location(),
-          '%<%=',
-          string.format('%%#%s#', hl_filename),
-          '%n',
-          ' ',
-        }, ''),
-        { scope = 'local' }
-      )
+      return
     end
+
+    local hl_background = focused and 'WinBar' or 'WinBarNC'
+    local hl_filename = 'WinBarNC'
+
+    if focused then
+      hl_filename = 'WinBarInfo'
+    end
+
+    if vim.bo.modified then
+      hl_filename = 'WinBarWarn'
+    end
+
+    pcall(
+      vim.api.nvim_set_option_value,
+      'winbar',
+      table.concat({
+        string.format('%%#%s#', hl_filename),
+        ' %.100t',
+        string.format('%%#%s#', hl_background),
+        theme.icons.separator,
+        vim.my.treesitter.gps.location(),
+        '%<%=',
+        string.format('%%#%s#', hl_filename),
+        '%n',
+        ' ',
+      }, ''),
+      { scope = 'local' }
+    )
   end
 end
 
