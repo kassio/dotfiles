b0811b3418c816f6dd7c654a2b7f89d91431fafe
Kassio Borges <kassioborgesm@gmail.com>
2022-07-27 21:40:17 +0200

    nvim: :nail_care: plugins file/load

--- stow/nvim/.config/nvim/lua/plugins/init.lua
+++ stow/nvim/.config/nvim/lua/plugins/init.lua
@@ -9,8 +9,9 @@ M.setup = function()
     P('Installing packer, after restarting nvim run :Upgrade')
     fn.system(string.format('git clone %s %s', repo, path))
 
-    M.sync()
-    vim.cmd('qall!')
+    M.upgrade({ bang = true })
+  else
+    M.load()
   end
 
   vim.api.nvim_create_user_command('Upgrade', require('plugins').upgrade, { bang = true })
@@ -51,42 +52,46 @@ M.load = function()
       use({ 'wbthomason/packer.nvim', opt = true })
 
       -- Statusline
-      use('nvim-lualine/lualine.nvim')
+      use({
+        'nvim-lualine/lualine.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons' },
+      })
 
       -- Project management
-      use('tpope/vim-projectionist')
+      use({ 'tpope/vim-projectionist' })
 
       -- Load configs from .editorconfig
-      use('gpanders/editorconfig.nvim')
+      use({ 'gpanders/editorconfig.nvim' })
 
       -- Restore last position
-      use('farmergreg/vim-lastplace')
+      use({ 'farmergreg/vim-lastplace' })
 
       -- Open file with file_path:line:path
-      use('wsdjeg/vim-fetch')
+      use({ 'wsdjeg/vim-fetch' })
 
       -- Comment management
-      use('numToStr/Comment.nvim')
+      use({ 'numToStr/Comment.nvim' })
 
       -- Replace variantions of a word
-      use('tpope/vim-abolish')
+      use({ 'tpope/vim-abolish' })
 
       -- Surround chars handling
-      use('kylechui/nvim-surround')
-      use('wellle/targets.vim')
-      use('tpope/vim-repeat') -- Enables repeat surround movements
+      use({ 'kylechui/nvim-surround' })
+
+      use({ 'wellle/targets.vim' })
+      use({ 'tpope/vim-repeat' }) -- Enables repeat surround movements
 
       -- text aligning
-      use('junegunn/vim-easy-align')
+      use({ 'junegunn/vim-easy-align' })
 
       -- File tree
       use({
         'kyazdani42/nvim-tree.lua',
-        requires = 'kyazdani42/nvim-web-devicons',
+        requires = { 'kyazdani42/nvim-web-devicons' },
       })
 
       -- Icons
-      use('kyazdani42/nvim-web-devicons')
+      use({ 'kyazdani42/nvim-web-devicons' })
 
       -- Fuzzy Finder
       use({
@@ -95,84 +100,95 @@ M.load = function()
         requires = { 'junegunn/fzf.vim' },
       })
 
-      use({ 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } })
-      use('nvim-lua/popup.nvim')
-      use('nvim-lua/plenary.nvim')
-      use('nvim-telescope/telescope-ui-select.nvim')
-      use({ 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' })
+      use({
+        {
+          'nvim-telescope/telescope.nvim',
+          requires = { 'nvim-lua/plenary.nvim' },
+        },
+        {
+          'nvim-telescope/telescope-fzf-native.nvim',
+          run = 'make',
+        },
+        'nvim-telescope/telescope-ui-select.nvim',
+      })
 
       -- Treesitter
-      use({ 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' })
-      use('nvim-treesitter/nvim-treesitter-textobjects')
-      use('nvim-treesitter/nvim-treesitter-refactor')
-      use('nvim-treesitter/playground')
-      use('lewis6991/spellsitter.nvim')
-      use('SmiteshP/nvim-gps')
+      use({
+        { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' },
+        'nvim-treesitter/nvim-treesitter-textobjects',
+        'nvim-treesitter/nvim-treesitter-refactor',
+        'nvim-treesitter/playground',
+        'lewis6991/spellsitter.nvim',
+        'SmiteshP/nvim-gps',
+      })
 
       -- LSP
-      use('neovim/nvim-lspconfig')
-      use('williamboman/nvim-lsp-installer')
-      -- generic LSP for diagnostic, formatting, etc
-      use('jose-elias-alvarez/null-ls.nvim')
-      -- symbols navigator
-      use('simrat39/symbols-outline.nvim')
+      use({
+        'neovim/nvim-lspconfig',
+        'williamboman/nvim-lsp-installer',
+        'jose-elias-alvarez/null-ls.nvim', -- generic LSP for diagnostic, formatting, etc
+        'simrat39/symbols-outline.nvim', -- symbols navigator
+      })
 
       -- Git
-      use('lewis6991/gitsigns.nvim')
-      use('emmanueltouzery/agitator.nvim')
+      use({ 'lewis6991/gitsigns.nvim', 'emmanueltouzery/agitator.nvim' })
 
       -- Completion
-      use('hrsh7th/nvim-cmp')
-      use('hrsh7th/cmp-nvim-lsp')
-      use('hrsh7th/cmp-buffer')
-      use('hrsh7th/cmp-path')
-      use('hrsh7th/cmp-nvim-lua')
-      use('f3fora/cmp-spell')
-      use('onsails/lspkind-nvim')
-      use('ray-x/cmp-treesitter')
-      use('hrsh7th/cmp-nvim-lsp-signature-help')
-      -- snippets
-      use('dcampos/nvim-snippy')
-      use('dcampos/cmp-snippy')
+      use({
+        'hrsh7th/nvim-cmp',
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-buffer',
+        'hrsh7th/cmp-path',
+        'hrsh7th/cmp-nvim-lua',
+        'f3fora/cmp-spell',
+        'onsails/lspkind-nvim',
+        'ray-x/cmp-treesitter',
+        'hrsh7th/cmp-nvim-lsp-signature-help',
+        -- snippets
+        'dcampos/nvim-snippy',
+        'dcampos/cmp-snippy',
+      })
 
-      use('folke/todo-comments.nvim')
+      use({ 'folke/todo-comments.nvim' })
 
       -- fancy notification
-      use('rcarriga/nvim-notify')
+      use({ 'rcarriga/nvim-notify' })
 
       -- terminal handling
-      use('kassio/neoterm')
+      use({ 'kassio/neoterm' })
 
       -- Split/Join commands
-      use('AndrewRadev/splitjoin.vim')
+      use({ 'AndrewRadev/splitjoin.vim' })
 
       -- Better pairs matching
-      use('andymass/vim-matchup')
+      use({ 'andymass/vim-matchup' })
 
       -- Test runner
-      use('vim-test/vim-test')
+      use({ 'vim-test/vim-test' })
 
       -- Debugger Tooling
-      use('mfussenegger/nvim-dap') -- debugger
-      use('rcarriga/nvim-dap-ui')
-      use('theHamsta/nvim-dap-virtual-text')
-      use('leoluz/nvim-dap-go') -- go debugger integration
+      use({
+        'mfussenegger/nvim-dap',
+        'rcarriga/nvim-dap-ui',
+        'theHamsta/nvim-dap-virtual-text',
+        'leoluz/nvim-dap-go', -- go debugger integration
+      })
 
       -- Theme
       use({ 'catppuccin/nvim', as = 'catppuccin' })
 
       -- Colors
-      use('norcalli/nvim-colorizer.lua') -- Highlight color strings
-      use('norcalli/nvim-terminal.lua') -- Fix terminal colors
-      use('https://gitlab.com/yorickpeterse/nvim-pqf.git') -- Prettier qf/loc windows
+      use({ 'norcalli/nvim-colorizer.lua' }) -- Highlight color strings
+      use({ 'norcalli/nvim-terminal.lua' }) -- Fix terminal colors
+      use({ 'https://gitlab.com/yorickpeterse/nvim-pqf.git' }) -- Prettier qf/loc windows
 
       -- Better language support
-      use('euclidianAce/BetterLua.vim') -- Lua
-      use('google/vim-jsonnet') -- Jsonnet
-      use('jparise/vim-graphql') -- Graphql
-      use('stephenway/postcss.vim') -- Postcss
-      use('tpope/vim-git') -- Postcss
-      use('fladson/vim-kitty') -- kitty terminal config
+      use({ 'euclidianAce/BetterLua.vim' }) -- Lua
+      use({ 'google/vim-jsonnet' }) -- Jsonnet
+      use({ 'jparise/vim-graphql' }) -- Graphql
+      use({ 'stephenway/postcss.vim' }) -- Postcss
+      use({ 'tpope/vim-git' }) -- Postcss
+      use({ 'fladson/vim-kitty' }) -- kitty terminal config
     end,
     config = {
       autoremove = true,
