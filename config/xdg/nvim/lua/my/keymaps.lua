0b30983148cdff02029340bd420e91cabf187ed6
Kassio Borges <kassioborgesm@gmail.com>
2022-09-11 18:24:47 +0100

    nvim: remove unnecessary code

--- config/xdg/nvim/lua/my/keymaps.lua
+++ config/xdg/nvim/lua/my/keymaps.lua
@@ -1,90 +1,91 @@
+local keymap = vim.keymap
 ------------------------------------------------------------
 -- Operation pending maps need to be passed as string
 -- expressions to vim, hence the double quote
 --
 -- select current line (inner)
-vim.keymap.set('x', 'il', ':<c-u>normal! g_v_<cr>')
-vim.keymap.set('o', 'il', '":normal vil<cr>"', { expr = true })
+keymap.set('x', 'il', ':<c-u>normal! g_v_<cr>')
+keymap.set('o', 'il', '":normal vil<cr>"', { expr = true })
 
 -- select current line (outer)
-vim.keymap.set('x', 'al', ':<c-u>normal! g_v0<cr>')
-vim.keymap.set('o', 'al', '":normal val<cr>"', { expr = true })
+keymap.set('x', 'al', ':<c-u>normal! g_v0<cr>')
+keymap.set('o', 'al', '":normal val<cr>"', { expr = true })
 
 -- select all lines
-vim.keymap.set('x', 'aF', ':<c-u>keepjumps normal! GVgg0<cr>')
-vim.keymap.set('o', 'aF', '":normal vaF<cr>"', { expr = true })
+keymap.set('x', 'aF', ':<c-u>keepjumps normal! GVgg0<cr>')
+keymap.set('o', 'aF', '":normal vaF<cr>"', { expr = true })
 ------------------------------------------------------------
 
 -- disable ex mode
-vim.keymap.set('n', [[Q]], [[<nop>]])
+keymap.set('n', [[Q]], [[<nop>]])
 
 -- allow gf to open non-existing files
-vim.keymap.set('n', 'gf', ':e <cfile><cr>')
+keymap.set('n', 'gf', ':e <cfile><cr>')
 
 -- use gj/gk by default to better navigation on wrapped lines
-vim.keymap.set('n', 'j', 'gj')
-vim.keymap.set('n', 'k', 'gk')
+keymap.set('n', 'j', 'gj')
+keymap.set('n', 'k', 'gk')
 
 -- faster esc
-vim.keymap.set('i', '<esc>', '<c-c>')
+keymap.set('i', '<esc>', '<c-c>')
 
 -- undo breakpoints
-vim.keymap.set('i', ',', ',<c-g>u')
-vim.keymap.set('i', '.', '.<c-g>u')
+keymap.set('i', ',', ',<c-g>u')
+keymap.set('i', '.', '.<c-g>u')
 
 -- keep the cursor centered while moving
-vim.keymap.set('n', 'n,', 'nzzzv')
-vim.keymap.set('n', 'N,', 'Nzzzv')
-vim.keymap.set('n', 'J,', 'mzJ`z')
+keymap.set('n', 'n,', 'nzzzv')
+keymap.set('n', 'N,', 'Nzzzv')
+keymap.set('n', 'J,', 'mzJ`z')
 
-vim.keymap.set('n', ']c', ']czz')
-vim.keymap.set('n', '[c', '[czz')
-vim.keymap.set('n', ']d', ']dzz')
-vim.keymap.set('n', '[d', '[dzz')
+keymap.set('n', ']c', ']czz')
+keymap.set('n', '[c', '[czz')
+keymap.set('n', ']d', ']dzz')
+keymap.set('n', '[d', '[dzz')
 
 -- escape from terminal mode with double <esc>
-vim.keymap.set('t', [[<esc><esc>]], [[<c-\><c-n>]])
+keymap.set('t', [[<esc><esc>]], [[<c-\><c-n>]])
 
 -- move to the last tab
-vim.keymap.set('n', '9gt', '<cmd>tablast<cr>')
+keymap.set('n', '9gt', '<cmd>tablast<cr>')
 
 -- paste without replacing the " register
-vim.keymap.set('v', '<leader>p', '"_dP')
+keymap.set('v', '<leader>p', '"_dP')
 
 -- search current word
-vim.keymap.set('n', '!', '"vyiw/\\V<c-r>v<cr>N')
+keymap.set('n', '!', '"vyiw/\\V<c-r>v<cr>N')
 -- search current selection
-vim.keymap.set('x', '!', '"vy/\\V<c-r>v<cr>N')
+keymap.set('x', '!', '"vy/\\V<c-r>v<cr>N')
 
 -- search current word case insensitive
-vim.keymap.set('n', '<leader>!', '"vyiw/\\V\\c<c-r>v<cr>N')
+keymap.set('n', '<leader>!', '"vyiw/\\V\\c<c-r>v<cr>N')
 -- search current selection case insensitive
-vim.keymap.set('x', '<leader>!', '"vy/\\V\\c<c-r>v<cr>N')
+keymap.set('x', '<leader>!', '"vy/\\V\\c<c-r>v<cr>N')
 
 -- search current word exclusive
-vim.keymap.set('n', 'g!', '"vyiw/\\V\\<<c-r>v\\><cr>N')
+keymap.set('n', 'g!', '"vyiw/\\V\\<<c-r>v\\><cr>N')
 -- search current selection exclusive
-vim.keymap.set('x', 'g!', '"vy/\\V\\<<c-r>v\\><cr>N')
+keymap.set('x', 'g!', '"vy/\\V\\<<c-r>v\\><cr>N')
 
 -- search current word case insensitive exclusive
-vim.keymap.set('n', '<leader>g!', '"vyiw/\\V\\c\\<<c-r>v\\><cr>N')
+keymap.set('n', '<leader>g!', '"vyiw/\\V\\c\\<<c-r>v\\><cr>N')
 -- search current selection case insensitive exclusive
-vim.keymap.set('x', '<leader>g!', '"vy/\\V\\c\\<<c-r>v\\><cr>N')
+keymap.set('x', '<leader>g!', '"vy/\\V\\c\\<<c-r>v\\><cr>N')
 
 -- delete current buffer
-vim.keymap.set('n', '<leader>bd', '<cmd>bw!<cr>')
+keymap.set('n', '<leader>bd', '<cmd>bw!<cr>')
 -- delete all buffers
-vim.keymap.set('n', '<leader>da', '<cmd>bufdo bw!<cr>')
+keymap.set('n', '<leader>da', '<cmd>bufdo bw!<cr>')
 
 -- indent current buffer
-vim.keymap.set('n', '<leader>ff', function()
+keymap.set('n', '<leader>ff', function()
   require('my.utils.buffers').preserve(function()
     vim.cmd([[normal! gg=G]])
   end)
 end)
 
 -- delete all buffers except current
-vim.keymap.set('n', '<leader>bo', function() -- delete all buffers but current
+keymap.set('n', '<leader>bo', function() -- delete all buffers but current
   local current = vim.api.nvim_win_get_buf(0)
   for _, buf in ipairs(vim.api.nvim_list_bufs()) do
     if buf ~= current then
@@ -94,7 +95,7 @@ vim.keymap.set('n', '<leader>bo', function() -- delete all buffers but current
 end)
 
 -- delete all hidden buffers
-vim.keymap.set('n', '<leader>dh', function() -- delete all hidden buffers
+keymap.set('n', '<leader>dh', function() -- delete all hidden buffers
   for _, buf in ipairs(vim.api.nvim_list_bufs()) do
     if vim.tbl_isempty(vim.fn.win_findbuf(buf)) and vim.api.nvim_buf_is_valid(buf) then
       vim.api.nvim_buf_delete(buf, { force = true })
@@ -103,7 +104,7 @@ vim.keymap.set('n', '<leader>dh', function() -- delete all hidden buffers
 end)
 
 -- close floating windows
-vim.keymap.set('n', '<leader>wa', function()
+keymap.set('n', '<leader>wa', function()
   for _, win in ipairs(vim.api.nvim_list_wins()) do
     local ok, config = pcall(vim.api.nvim_win_get_config, win)
 
@@ -114,11 +115,11 @@ vim.keymap.set('n', '<leader>wa', function()
 end)
 
 -- open the url under the cursor
-vim.keymap.set('n', 'gx', function()
+keymap.set('n', 'gx', function()
   vim.fn.jobstart('open ' .. vim.fn.expand('<cfile>'))
 end, { silent = true })
 
-vim.keymap.set('x', 'gx', function()
+keymap.set('x', 'gx', function()
   vim.cmd('normal! "vy')
   local uri = vim.fn.getreg('v')
   vim.fn.jobstart('open ' .. string.gsub(uri, '%s', ''))
--- config/xdg/nvim/lua/my/options.lua
+++ config/xdg/nvim/lua/my/options.lua
@@ -1,18 +1,30 @@
 local opt = vim.opt
-local g = vim.g
+
+-- Space as map leader
+vim.g.mapleader = ' '
 
 opt.autowriteall = true
 opt.backup = false
 opt.cindent = true
 opt.colorcolumn = { 80, 100, 120 }
 opt.complete:remove('t')
-opt.completeopt = { 'menu', 'menuone', 'preview', 'noselect' }
+opt.completeopt = {
+  'menu',
+  'menuone',
+  'preview',
+  'noselect',
+}
 opt.confirm = true
 opt.copyindent = true
 opt.cursorline = true
 opt.cursorlineopt = 'number'
 opt.dictionary = '/usr/share/dict/words'
-opt.diffopt = { 'internal', 'filler', 'closeoff', 'vertical' }
+opt.diffopt = {
+  'internal',
+  'filler',
+  'closeoff',
+  'vertical',
+}
 opt.expandtab = true
 opt.fileencodings = { 'utf8', 'nobomb' }
 opt.fileformats = { 'unix', 'mac' }
@@ -66,30 +78,3 @@ opt.virtualedit = 'block'
 opt.wildignorecase = true
 opt.wrap = false
 opt.writebackup = false
-
--- Space as map leader
-g.mapleader = ' '
-
--- Disable standard plugins
-g.loaded_gzip = 1
-g.loaded_zip = 1
-g.loaded_zipPlugin = 1
-g.loaded_tar = 1
-g.loaded_tarPlugin = 1
-
-g.loaded_getscript = 1
-g.loaded_getscriptPlugin = 1
-g.loaded_vimball = 1
-g.loaded_vimballPlugin = 1
-g.loaded_2html_plugin = 1
-
-g.loaded_matchit = 1
-g.loaded_matchparen = 1
-g.loaded_logiPat = 1
-g.loaded_rrhelper = 1
-
-g.loaded_netrw = 1
-g.loaded_netrwPlugin = 1
-g.loaded_netrwSettings = 1
-
-g.do_filetype_lua = 1
