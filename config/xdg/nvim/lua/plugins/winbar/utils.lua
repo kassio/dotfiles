b3fd20cb1e3e249dd1e1530d2c8facc3052c477d
Kassio Borges <kassioborgesm@gmail.com>
2022-09-11 14:41:42 +0100

    nvim: ensure winbar title size

--- config/xdg/nvim/lua/plugins/winbar/utils.lua
+++ config/xdg/nvim/lua/plugins/winbar/utils.lua
@@ -1,5 +1,22 @@
 local utils = require('my.utils')
 
+local get_name = function(bufnr)
+  local fullname = vim.api.nvim_buf_get_name(bufnr)
+
+  if #fullname == 0 then
+    return '[No name]'
+  else
+    local name = vim.fn.fnamemodify(fullname, ':.')
+
+    if #name > 70 then
+      local parts = vim.split(name, '/')
+      return '.../' .. table.concat(parts, '/', 2, #parts)
+    end
+
+    return name
+  end
+end
+
 return {
   render = function()
     local ft = vim.bo.filetype
@@ -10,6 +27,7 @@ return {
 
     local bg_hl = 'Winbar'
     local bufnr = vim.api.nvim_get_current_buf()
+    local name = get_name(bufnr)
     local icon, icon_hl = utils.buffers.fileicon_extend_hl(bufnr, bg_hl)
 
     if tonumber(vim.g.actual_curbuf) ~= tonumber(bufnr) then
@@ -21,10 +39,10 @@ return {
       string.format('%%#%s#', bg_hl),
       '%=',
       string.format('%%#%s#%s%%*', icon_hl, icon),
-      ' %f',
-      ' %n',
+      name,
+      '%n',
       '%=',
-    }, '')
+    }, ' ')
 
     return winbar
   end,
