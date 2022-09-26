7cd9f9f5ef3d187e9b2c468496327f34ebc8e98c
Kassio Borges <kassioborgesm@gmail.com>
2022-09-26 20:01:45 +0100

    nvim: fix ruby icons

--- config/xdg/nvim/lua/plugins/icons.lua
+++ config/xdg/nvim/lua/plugins/icons.lua
@@ -1,9 +1,24 @@
 local devicons = require('nvim-web-devicons')
 local icons = devicons.get_icons()
 
+icons['rb'] = {
+  icon = '',
+  color = '#701516',
+  cterm_color = '52',
+  name = 'rb',
+}
+
+local ext = function(source, target)
+  vim.tbl_extend('force', icons[source], { name = target })
+end
+
 devicons.setup({
   override = {
-    ruby = icons['rb'],
+    rb = icons['rb'],
+    ruby = ext('rb', 'ruby'),
+    irbrc = ext('rb', 'irbrc'),
+    pryrc = ext('rb', 'pryrc'),
+    Brewfile = ext('rb', 'Brewfile'),
     go = icons['go'],
     sh = icons['sh'],
     bash = icons['bash'],
