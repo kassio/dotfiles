c7b0f82143a620ab42bedb871c952572678fa336
Kassio Borges <kassioborgesm@gmail.com>
2022-10-07 18:51:22 +0100

    nvim: avoid error with _G.R

--- config/xdg/nvim/lua/globals.lua
+++ config/xdg/nvim/lua/globals.lua
@@ -17,7 +17,10 @@ end
 -- Reloads a package by name
 -- If it fails to be loaded the error is printed
 R = function(name)
-  package.loaded[name] = nil
+  if package.loaded[name] ~= nil then
+    package.loaded[name] = nil
+  end
+
   local status, result = pcall(require, name)
 
   if status then
