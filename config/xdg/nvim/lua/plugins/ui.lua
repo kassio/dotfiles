731ec542f02f982c0f24d065e6f9f9a03e8322da
Kassio Borges <kassioborgesm@gmail.com>
2022-10-19 13:57:43 +0100

    nvim: tweaking ui popups and notifications

--- config/xdg/nvim/lua/plugins/ui.lua
+++ config/xdg/nvim/lua/plugins/ui.lua
@@ -1,10 +1,40 @@
 vim.opt.cmdheight = 0
 
 require('noice').setup({
+  views = {
+    cmdline_popup = {
+      position = {
+        row = 3,
+        col = '50%',
+      },
+      size = {
+        width = 80,
+        height = 'auto',
+      },
+    },
+    popupmenu = {
+      relative = 'editor',
+      position = {
+        row = 6,
+        col = '50.5%',
+      },
+      size = {
+        width = 82,
+        height = 10,
+      },
+      border = {
+        style = 'rounded',
+        padding = { 0, 1 },
+      },
+      win_options = {
+        winhighlight = { Normal = 'Normal', FloatBorder = 'DiagnosticInfo' },
+      },
+    },
+  },
   routes = {
     {
       view = 'split',
-      filter = { event = 'msg_show', min_height = 10 },
+      filter = { event = 'msg_show', min_height = 3 },
     },
     {
       filter = {
