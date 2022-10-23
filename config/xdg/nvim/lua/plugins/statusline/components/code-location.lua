e6161f037a909a6913df3156b3c8601d8a033274
Kassio Borges <kassioborgesm@gmail.com>
2022-10-23 20:59:41 +0100

    nvim: statusline make code-location clickable

--- config/xdg/nvim/lua/plugins/statusline/components/code-location.lua
+++ config/xdg/nvim/lua/plugins/statusline/components/code-location.lua
@@ -1,7 +1,16 @@
 local utils = require('my.utils')
 
 return {
+  on_click = function(_bufnr, _clicks, _button, mods)
+    utils.to_clipboard(utils.treesitter.location(), vim.fn.stridx(mods, 'c') ~= -1)
+  end,
   render = function()
-    return string.format(' %s ', utils.treesitter.location())
+    local bufnr = vim.api.nvim_get_current_buf()
+
+    return string.format(
+      " %%%d@v:lua.require'plugins.statusline.components.code-location'.on_click@%s%%X ",
+      bufnr,
+      utils.treesitter.location()
+    )
   end,
 }
