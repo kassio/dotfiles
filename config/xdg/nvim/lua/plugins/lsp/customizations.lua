50acacccd1dd824d2fe528c7b582e6995fa059ca
Kassio Borges <kassioborgesm@gmail.com>
2022-10-24 15:59:20 +0100

    nvim: do not warn about unused var prefixed with _

--- config/xdg/nvim/lua/plugins/lsp/customizations.lua
+++ config/xdg/nvim/lua/plugins/lsp/customizations.lua
@@ -14,6 +14,9 @@ return {
             'vim',
             'require',
           },
+          unusedLocalExclude = {
+            '_*',
+          },
         },
         workspace = {
           -- Make the server aware of Neovim runtime files
