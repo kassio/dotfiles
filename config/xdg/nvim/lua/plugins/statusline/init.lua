3b4fab434052d6e935032201f3baa8c252141b3c
Kassio Borges <kassioborgesm@gmail.com>
2023-01-08 17:22:07 -0300

    nvim: rename statusline.utils to statusline.renderer

--- config/xdg/nvim/lua/plugins/statusline/init.lua
+++ config/xdg/nvim/lua/plugins/statusline/init.lua
@@ -1,2 +1,2 @@
 vim.opt.laststatus = 3
-vim.opt.statusline = '%{%v:lua.require("plugins.statusline.utils").render()%}'
+vim.opt.statusline = '%{%v:lua.require("plugins.statusline.renderer").render()%}'
