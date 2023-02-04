915e40941e364149348bb69b3688bc975e0f71da
Kassio Borges <kassioborgesm@gmail.com>
2023-02-04 17:35:23 -0500

    nvim: adding some more basic queries (copy from nvim-gps)

--- config/xdg/nvim/after/queries/lua/location.scm
+++ config/xdg/nvim/after/queries/lua/location.scm
@@ -1,40 +1,40 @@
 ; Functions
 ((function_declaration
-	name: (identifier) @function-name) @root)
+   name: (identifier) @function) @root)
 
 ((function_declaration
-	name: (dot_index_expression) @table-function) @root)
+   name: (dot_index_expression) @function) @root)
 
 ; Function assined to variables
 ((assignment_statement
-	(variable_list
-		name: (identifier) @function-name)
-	(expression_list
-		value: (function_definition))) @root)
+   (variable_list
+     name: (identifier) @function)
+   (expression_list
+     value: (function_definition))) @root)
 
 ((assignment_statement
-	(variable_list
-		name: (dot_index_expression) @table-function)
-	(expression_list
-		value: (function_definition))) @root)
+   (variable_list
+     name: (dot_index_expression) @function)
+   (expression_list
+     value: (function_definition))) @root)
 
 ; Methods
 ((field
-	name: (identifier) @method-name
-	value: (function_definition)) @root)
+   name: (identifier) @function
+   value: (function_definition)) @root)
 
 ((field
-	name: (string) @string-method
-	value: (function_definition)) @root)
+   name: (string) @function
+   value: (function_definition)) @root)
 
 ; Tables
 ((assignment_statement
-	(variable_list
-		name: (_) @multi-container)
-	(expression_list
-		value: (table_constructor))) @root)
+   (variable_list
+     name: (_) @container)
+   (expression_list
+     value: (table_constructor))) @root)
 
 ; Table inside table
 ((field
-	name: (identifier) @container-name
-	value: (table_constructor)) @root)
+   name: (identifier) @container
+   value: (table_constructor)) @root)
--- config/xdg/nvim/lua/config/hbar/components/treesitter-location/processors/init.lua
+++ config/xdg/nvim/lua/config/hbar/components/treesitter-location/processors/init.lua
@@ -1,4 +1,18 @@
+local default = function(nodes)
+  local text = ''
+
+  for _, node in ipairs(nodes) do
+    local _, value = table.unpack(node)
+
+    text = text .. ' » ' .. value
+  end
+
+  return text
+end
+
 return {
-  lua = require('config.hbar.components.treesitter-location.processors.lua'),
+  go = default,
+  javascript = default,
+  lua = default,
   ruby = require('config.hbar.components.treesitter-location.processors.ruby'),
 }
