df53387cdb9906334cf1ba1ec510efd392a250f1
Kassio Borges <kassioborgesm@gmail.com>
2023-03-30 18:10:13 +0100

    nvim: add :GoAcceptance to run individual golang acceptance tests

--- config/xdg/nvim/after/ftplugin/go.lua
+++ config/xdg/nvim/after/ftplugin/go.lua
@@ -8,3 +8,12 @@ dap_go.setup()
 
 vim.api.nvim_create_user_command('GoDebugTest', dap_go.debug_test, {})
 vim.keymap.set('n', '<leader>dt', '<cmd>GoDebugTest<cr>')
+
+vim.api.nvim_create_user_command('GoAcceptance', function(cmd)
+  local cmds = {
+    'clear',
+    'make',
+    string.format("GOFLAGS='-count=1' go test -v -run '%s' ./test/acceptance", cmd.args),
+  }
+  vim.cmd.T(table.concat(cmds, ' && '))
+end, { nargs = 1 })
