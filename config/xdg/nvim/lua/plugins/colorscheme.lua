3d673c135ae4eaf8ae19b4863bc77169ed4e8b8a
Kassio Borges <kassioborgesm@gmail.com>
2023-04-16 09:31:33 +0100

    nvim: :nail_care:

--- config/xdg/nvim/lua/plugins/colorscheme.lua
+++ config/xdg/nvim/lua/plugins/colorscheme.lua
@@ -4,24 +4,22 @@ return {
     name = 'catppuccin',
     priority = 1000,
     config = function()
-      local highlights = require('plugins.ui.config.highlights')
-
       require('catppuccin').setup({
-        opts = {
-          flavour = 'mocha',
-          bg = {
-            light = 'latte',
-            dark = 'mocha',
-          },
-          integrations = {
-            cmp = true,
-            nvimtree = true,
-            telescope = true,
-            notify = true,
-            mason = true,
-          },
+        flavour = 'mocha',
+        bg = {
+          light = 'latte',
+          dark = 'mocha',
         },
-        custom_highlights = highlights,
+        integrations = {
+          cmp = true,
+          mason = true,
+          notify = true,
+          nvimtree = true,
+          telescope = true,
+        },
+        show_end_of_buffer = false,
+        term_colors = false,
+        custom_highlights = require('plugins.ui.config.highlights'),
       })
 
       vim.cmd.colorscheme('catppuccin')
