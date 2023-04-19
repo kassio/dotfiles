9b2d82501a261083d43d72442659c4a30a3d0786
Kassio Borges <kassioborgesm@gmail.com>
2023-04-19 16:27:26 +0100

    nvim: add tabulirize on easyaline description for easier finding

--- config/xdg/nvim/lua/plugins/text.lua
+++ config/xdg/nvim/lua/plugins/text.lua
@@ -9,10 +9,19 @@ return {
     'junegunn/vim-easy-align',
     config = function()
       -- Start interactive EasyAlign in visual mode (e.g. vipga)
-      vim.keymap.set('x', 'ga', '<Plug>(EasyAlign)', { desc = 'aligner: start aligning' })
+      vim.keymap.set(
+        'x',
+        'ga',
+        '<Plug>(EasyAlign)',
+        { desc = 'aligner: start aligning | tabularize' }
+      )
       -- Start interactive EasyAlign for a motion/text object (e.g. gaip)
-      vim.keymap.set('n', 'ga', '<Plug>(EasyAlign)', { desc = 'aligner: start aligning' })
+      vim.keymap.set(
+        'n',
+        'ga',
+        '<Plug>(EasyAlign)',
+        { desc = 'aligner: start aligning | tabularize' }
+      )
     end,
   },
-
 }
