394d449412e2350ab90a38c36a714ae7507a80a2
Kassio Borges <kassioborgesm@gmail.com>
2023-04-22 10:48:22 +0200

    when updating iterm2 theme also update nvim backgrounds

---
+++ config/iterm2scripts/AutoLaunch/theme.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3.7
+
+import iterm2
+import os
+
+async def main(connection):
+    app = await iterm2.async_get_app(connection)
+
+    def home(str):
+      return "{}/{}".format(os.environ["HOME"], str)
+
+    def writeCache(name):
+        f = open(home(".cache/iterm_profile"), "w")
+        f.write(name)
+        f.close()
+
+    def updateNvimSessions(name):
+        bin = home(".dotfiles/bin/for-each-nvim 'set bg={}'").format(name)
+        os.system(bin)
+
+    async def setProfile(name):
+        partialProfiles = await iterm2.PartialProfile.async_query(connection)
+        currentTab = app.current_terminal_window.current_tab
+
+        for partial in partialProfiles:
+            if partial.name == name:
+                profile = await partial.async_get_full_profile()
+                await currentTab.current_session.async_set_profile(profile)
+                writeCache(name)
+                updateNvimSessions(name)
+                return
+
+    @iterm2.RPC
+    async def profileDark():
+        await setProfile("dark")
+
+    await profileDark.async_register(connection)
+
+    @iterm2.RPC
+    async def profileLight():
+        await setProfile("light")
+
+    await profileLight.async_register(connection)
+
+iterm2.run_forever(main)
