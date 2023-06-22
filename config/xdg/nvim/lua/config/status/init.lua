9f14b59aa7725c1aa3f136b9431f353d18f94ab9
Kassio Borges <kassioborgesm@gmail.com>
2023-06-22 15:28:02 +0100

    nvim: warn about failed status loading modules

--- config/xdg/nvim/lua/config/status/init.lua
+++ config/xdg/nvim/lua/config/status/init.lua
@@ -1,7 +1,14 @@
+local warn_icon = require('utils.highlights').get_sign_icon('warn')
 local opt = vim.opt
 
 local function render(name)
-  return string.format('%%{%%v:lua.require("config.status.%s").render()%%}', name)
+  local ok, text = pcall(string.format, '%%{%%v:lua.require("config.status.%s").render()%%}', name)
+
+  if not ok then
+    return string.format('%s %s %s', warn_icon, name, warn_icon)
+  else
+    return text
+  end
 end
 
 -- statuscolumn
