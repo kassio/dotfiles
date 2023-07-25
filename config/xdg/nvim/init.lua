21386b5a3c69f6c14bf44b5c4b140c84e84233c0
Kassio Borges <kassioborgesm@gmail.com>
2023-07-25 15:11:42 +0100

    nvim: add forward compatibility with nvim0.10
    - vim.ui.open

--- config/xdg/nvim/init.lua
+++ config/xdg/nvim/init.lua
@@ -6,4 +6,9 @@ require('globals')
 -- my config
 require('config')
 
+-- To be removed with nvim 0.10
+vim.ui.open = function(value)
+  vim.fn.jobstart('open ' .. value)
+end
+
 pcall(vim.cmd.source, vim.env.HOME .. '/.nvim.lua')
