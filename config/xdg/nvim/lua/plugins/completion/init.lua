dc232a664b0b4fe1c000db73739c9fb23e97bf69
Kassio Borges <kassioborgesm@gmail.com>
2023-08-22 18:32:48 +0100

    nvim: enable buffer autocomplete based on existing windows

--- config/xdg/nvim/lua/plugins/completion/init.lua
+++ config/xdg/nvim/lua/plugins/completion/init.lua
@@ -58,8 +58,19 @@ return {
         { name = 'nvim_lsp_signature_help' },
         { name = 'snippy' },
       }, {
+        {
+          name = 'buffer',
+          option = {
+            get_bufnrs = function()
+              local bufs = {}
+              for _, win in ipairs(vim.api.nvim_list_wins()) do
+                bufs[vim.api.nvim_win_get_buf(win)] = true
+              end
+              return vim.tbl_keys(bufs)
+            end,
+          },
+        },
         { name = 'treesitter' },
-        { name = 'buffer' },
         { name = 'spell' },
       }, {
         { name = 'nvim_lua' },
