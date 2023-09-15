3aa592eb69839aed1761fbebb1dd3c5520b62033
Kassio Borges <kassioborgesm@gmail.com>
2023-09-15 19:40:06 +0200

    nvim: fix rubocop linter
    Related to: https://github.com/nvimdev/guard.nvim/issues/104

--- config/xdg/nvim/lua/plugins/lsp/init.lua
+++ config/xdg/nvim/lua/plugins/lsp/init.lua
@@ -29,10 +29,43 @@ return {
     },
     config = function()
       local ft = require('guard.filetype')
+      local lint = require('guard.lint')
 
       ft('go'):fmt('lsp')
       ft('lua'):fmt('stylua'):lint('luacheck'):append('lsp')
-      ft('ruby'):lint('rubocop')
+      ft('ruby'):lint({
+        cmd = 'bundle',
+        args = { 'exec', 'rubocop', '--format', 'json', '--force-exclusion', '--stdin' },
+        stdin = true,
+        parse = function(result, bufnr)
+          local decoded = vim.json.decode(result)
+          local diagnostics = {}
+          local severities = {
+            convention = lint.severities.info,
+            error = lint.severities.error,
+            fatal = lint.severities.fatal,
+            info = lint.severities.info,
+            refactor = lint.severities.style,
+            warning = lint.severities.warning,
+          }
+
+          for _, diagnostic in ipairs(decoded.files[1].offenses) do
+            table.insert(diagnostics, {
+              bufnr = bufnr,
+              source = diagnostic.cop_name,
+              lnum = diagnostic.location.start_line,
+              end_lnum = diagnostic.location.last_line,
+              col = diagnostic.location.start_column,
+              end_col = diagnostic.location.last_column,
+              code = diagnostic.cop_name,
+              severity = severities[diagnostic.severity],
+              message = diagnostic.message,
+            })
+          end
+
+          return diagnostics
+        end,
+      })
       ft('sh'):fmt('shfmt'):lint('shellcheck')
 
       require('guard').setup({
