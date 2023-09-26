c4c92c9e388627edcb1ef0e021a00d99794282cc
Kassio Borges <kassioborgesm@gmail.com>
2023-09-26 20:52:44 +0200

    nvim: make more neoterm commands work on remote

--- config/xdg/nvim/lua/plugins/terminal.lua
+++ config/xdg/nvim/lua/plugins/terminal.lua
@@ -11,50 +11,40 @@ return {
       local g = vim.g
       local neoterm_server_addr = vim.fs.joinpath(vim.fn.stdpath('cache'), 'neoterm_server.pipe')
 
-      local keymap = function(key, command, desc)
+      local keymap = function(desc, key, command)
         vim.keymap.set('n', key, command, { desc = 'neoterm: ' .. desc })
       end
 
-      local neoterm = function(cmdRemote, cmdLocal)
+      local neoterm_fn = function(fn, opts)
         if vim.g.neoterm_server == true or vim.fn.filereadable(neoterm_server_addr) == 0 then
-          if cmdLocal ~= nil then
-            cmdLocal()
-          else
-            vim.fn['neoterm#do']({ cmd = cmdRemote })
-          end
+          vim.fn['neoterm#' .. fn](opts)
         else
-          local cmd = {
+          local args = ''
+          if opts ~= nil then
+            args = vim.json.encode(opts)
+          end
+
+          vim.system({
             'nvim',
             '--server',
             neoterm_server_addr,
             '--remote-send',
-            string.format([[<C-\><C-N>:T %s<CR>]], cmdRemote),
-          }
-
-          vim.system(cmd)
+            string.format([[<C-\><C-N>:call neoterm#%s(%s)<CR>]], fn, args),
+          })
         end
       end
 
-      keymap('<leader>te', '<cmd>T exit<cr>', 'exit')
-      keymap('<leader>tg', '<cmd>Tredo<cr>', 'redo')
-      keymap('<leader>th', '<cmd>botright Ttoggle<cr>', 'horizontal toggle')
-      keymap('<leader>tk', '<cmd>Tkill<cr>', 'kill (send ctrl_c)')
-      keymap('<leader>tl', '<cmd>T clear<cr>', 'clear')
-      keymap('<leader>tm', vim.fn['neoterm#map_do'], 'run command saved with Tmap')
-      keymap('<leader>tt', '<cmd>Ttoggle<cr>', 'toggle')
-      keymap('<leader>tv', '<cmd>botright vertical Ttoggle<cr>', 'vertical toggle')
+      local neoterm_cmd = function(opts)
+        neoterm_fn('do', {
+          cmd = opts.args,
+          target = opts.count or 0,
+          mod = opts.mods or '',
+        })
+      end
 
-      keymap('<leader>tL', function()
-        neoterm('clear', function()
-          local bufnr = vim.g.neoterm.instances['1'].buffer_id
-          local original = vim.api.nvim_get_option_value('scrollback', { buf = bufnr })
-          vim.api.nvim_set_option_value('scrollback', 0, { buf = bufnr })
-          vim.uv.sleep(100)
-          vim.cmd.T('clear')
-          vim.uv.sleep(100)
-          vim.api.nvim_set_option_value('scrollback', original, { buf = bufnr })
-        end)
-      end, 'clear buffer')
+      vim.api.nvim_create_user_command('T', function(opts)
+        neoterm_cmd(opts)
+      end, { nargs = '+' })
 
       vim.api.nvim_create_user_command('Tcd', function(opts)
         local pwd = vim.fn.getcwd()
@@ -62,12 +52,8 @@ return {
           pwd = opts.args
         end
 
-        vim.cmd.T('cd ' .. pwd)
+        neoterm_cmd({ args = 'cd ' .. pwd })
       end, { nargs = '?' })
-
-      vim.api.nvim_create_user_command('T', function(opts)
-        neoterm(opts.args)
-      end, { nargs = '+' })
 
       vim.api.nvim_create_user_command('NeotermServerStart', function()
         vim.fn.serverstart(neoterm_server_addr)
@@ -75,6 +61,29 @@ return {
         vim.cmd.bwipeout()
         vim.g.neoterm_server = true
       end, {})
+
+      keymap('horizontal toggle', '<leader>th', '<cmd>botright Ttoggle<cr>')
+      keymap('toggle', '<leader>tt', '<cmd>Ttoggle<cr>')
+      keymap('vertical toggle', '<leader>tv', '<cmd>botright vertical Ttoggle<cr>')
+
+      keymap('exit', '<leader>te', function()
+        neoterm_cmd({ args = 'exit' })
+      end)
+      keymap('redo', '<leader>tg', function()
+        neoterm_fn('redo', { count = 0 })
+      end)
+      keymap('kill (send ctrl_c)', '<leader>tk', function()
+        neoterm_fn('kill', { count = 0 })
+      end)
+      keymap('clear', '<leader>tl', function()
+        neoterm_fn('clear', { force_clear = 0 })
+      end)
+      keymap('erase buffer', '<leader>tL', function()
+        neoterm_fn('clear', { force_clear = 1 })
+      end)
+      keymap('run command saved with Tmap', '<leader>tm', function()
+        neoterm_cmd({ args = vim.tbl_get(vim.g, 'neoterm', 'map_options', 'cmd') })
+      end)
 
       g.neoterm_default_mod = 'botright'
       g.neoterm_automap_keys = ''
