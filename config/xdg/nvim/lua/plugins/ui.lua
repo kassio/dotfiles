69fdd37ea09e9266736c42dc3273b2df703a1c85
Kassio Borges <kassioborgesm@gmail.com>
2023-09-26 19:46:57 +0200

    nvim: add marks.nvim

--- config/xdg/nvim/lua/plugins/ui.lua
+++ config/xdg/nvim/lua/plugins/ui.lua
@@ -71,6 +71,21 @@ return {
     end,
   },
 
+  {
+    'chentoast/marks.nvim',
+    config = function()
+      require('marks').setup({
+        default_mappings = true,
+        builtin_marks = { '.', '<', '>', '^' },
+        cyclic = true,
+        force_write_shada = false,
+        refresh_interval = 250,
+        sign_priority = { lower = 10, upper = 15, builtin = 8, bookmark = 20 },
+        excluded_filetypes = {},
+      })
+    end,
+  },
+
   -- vim.ui.input & vim.ui.select
   {
     'stevearc/dressing.nvim',
