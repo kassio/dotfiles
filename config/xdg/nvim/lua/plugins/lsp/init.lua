9b48be8ce24021c7045d008a5d74e6cd6cd62d7b
Kassio Borges <kassioborgesm@gmail.com>
2023-09-29 19:39:13 +0200

    nvim: move lsp.autoformat code to its own module

--- config/xdg/nvim/lua/plugins/lsp/init.lua
+++ config/xdg/nvim/lua/plugins/lsp/init.lua
@@ -16,31 +16,7 @@ return {
     config = function()
       require('plugins.lsp.handlers').setup()
       require('plugins.lsp.servers').setup()
-
-      local function lsp_format(bufnr)
-        if not vim.bo[bufnr].modifiable or vim.b[bufnr].skip_autoformat == true then
-          return
-        end
-
-        local efm = vim.lsp.get_clients({ name = 'efm' })
-
-        if vim.tbl_isempty(efm) then
-          vim.lsp.buf.format()
-        else
-          vim.lsp.buf.format({ name = 'efm' })
-        end
-      end
-
-      vim.api.nvim_create_user_command('LspFormat', function()
-        lsp_format(vim.api.nvim_get_current_buf())
-      end, {})
-
-      vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
-        group = vim.api.nvim_create_augroup('user:lsp', { clear = false }),
-        callback = function(args)
-          lsp_format(args.buf)
-        end,
-      })
-    end,
+      require('plugins.lsp.autoformat').setup()
+    end
   },
 }
