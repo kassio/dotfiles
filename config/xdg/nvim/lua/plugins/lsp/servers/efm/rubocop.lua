03762842048da3d7352233452bf510841b8e98c2
Kassio Borges <kassioborgesm@gmail.com>
2023-10-04 15:43:52 +0200

    nvim: simplify efm rubocop config

--- config/xdg/nvim/lua/plugins/lsp/servers/efm/rubocop.lua
+++ config/xdg/nvim/lua/plugins/lsp/servers/efm/rubocop.lua
@@ -1,16 +1,16 @@
-local config = require('efmls-configs.linters.rubocop')
 local fs = require('efmls-configs.fs')
+local rubocop = require('efmls-configs.linters.rubocop')
 local cmd = fs.executable('rubocop', fs.Scope.BUNDLE)
 
-config.lintCommand = string.format('%s --format emacs --stdin "${INPUT}"', cmd)
-config.lintCategoryMap = {
-  A = 'H', -- autocorrect
-  C = 'I', -- convention
-  E = 'E', -- error
-  F = 'E', -- fatal
-  I = 'I', -- info
-  R = 'H', -- refactor
-  W = 'W', -- warning
-}
-
-return config
+return vim.tbl_extend('force', rubocop, {
+  lintCommand = string.format('%s --format emacs --stdin "${INPUT}"', cmd),
+  lintCategoryMap = {
+    A = 'H', -- autocorrect
+    C = 'I', -- convention
+    E = 'E', -- error
+    F = 'E', -- fatal
+    I = 'I', -- info
+    R = 'H', -- refactor
+    W = 'W', -- warning
+  },
+})
