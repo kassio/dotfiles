e606c6c96d7d65e6983b3f129c2b96ff8582816a
Kassio Borges <kassioborgesm@gmail.com>
2023-10-19 19:48:10 +0100

    Ensure to use latest version of gitlab

--- config/xdg/nvim/lua/plugins/gitlab.lua
+++ config/xdg/nvim/lua/plugins/gitlab.lua
@@ -1,9 +1,11 @@
 return {
   {
     url = 'https://gitlab.com/gitlab-org/editor-extensions/gitlab.vim.git',
+    branch = 'main',
     lazy = false,
     config = function()
       require('gitlab').setup({
+        minimal_message_level = vim.lsp.log_levels.WARN,
         statusline = {
           enabled = false,
         },
