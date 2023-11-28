a87c39b5ebd65220374e0d9c8198323551775b0b
Kassio Borges <kassioborgesm@gmail.com>
2023-11-28 17:01:23 +0000

    wezterm: add a menu to move pane to new window

--- config/xdg/wezterm/palette.lua
+++ config/xdg/wezterm/palette.lua
@@ -3,11 +3,18 @@ return {
     wezterm.on('augment-command-palette', function(_w, _p)
       return {
         {
-          brief = 'Move pane to new tab',
+          brief = 'Pane: Move to new tab',
           icon = 'md_tab',
-
           action = wezterm.action_callback(function(_window, pane)
             local tab = pane:move_to_new_tab()
+            tab:activate()
+          end),
+        },
+        {
+          brief = 'Pane: Move to new window',
+          icon = 'md_window_restore',
+          action = wezterm.action_callback(function(_window, pane)
+            local tab = pane:move_to_new_window()
             tab:activate()
           end),
         },
