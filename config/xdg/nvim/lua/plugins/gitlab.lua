3b5569eb62532d2c1714ac1270ab03296d4dbda2
Kassio Borges <kassioborgesm@gmail.com>
2023-12-15 21:56:19 +0000

    update karabiner config

--- config/xdg/nvim/lua/plugins/gitlab.lua
+++ config/xdg/nvim/lua/plugins/gitlab.lua
@@ -1,15 +1,24 @@
 return {
   {
     url = 'https://gitlab.com/gitlab-org/editor-extensions/gitlab.vim.git',
-    branch = 'main',
-    lazy = false,
-    config = function()
-      require('gitlab').setup({
-        minimal_message_level = vim.lsp.log_levels.WARN,
-        statusline = {
-          enabled = false,
-        },
-      })
+    event = {
+      'BufReadPre',
+      'BufNewFile',
+    },
+    ft = {
+      'go',
+      'javascript',
+      'python',
+      'ruby',
+    },
+    cond = function()
+      return vim.env.GITLAB_TOKEN ~= nil and vim.env.GITLAB_TOKEN ~= ''
     end,
+    opts = {
+      minimal_message_level = vim.lsp.log_levels.WARN,
+      statusline = {
+        enabled = false,
+      },
+    },
   },
 }
