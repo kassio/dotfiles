924f1ee9c13f8feb7d25db75cd5d83694bd1a950
Kassio Borges <kassioborgesm@gmail.com>
2023-12-20 16:19:28 +0000

    nvim: tweak completion format

--- config/xdg/nvim/lua/plugins/completion/formatting.lua
+++ config/xdg/nvim/lua/plugins/completion/formatting.lua
@@ -1,11 +1,11 @@
 local source_labels = {
   buffer = '',
   bug = '',
-  nvim_lsp = '',
+  nvim_lsp = '󰒍',
   nvim_lua = '',
-  path = 'פּ',
+  path = '󰇐',
   snippy = '',
-  spell = '暈',
+  spell = '',
   treesitter = '',
 }
 
@@ -44,17 +44,19 @@ local function ensure_lengh(content, length)
   return content .. string.rep(' ', length - #content)
 end
 
-local function icon(item, entry)
-  local label = kind_labels[item.kind] or source_labels[entry.source.name] or ' '
+local function icon(entry, item)
+  local default = ' '
+  local source = source_labels[entry.source.name] or default
+  local kind = kind_labels[item.kind] or source
 
-  return string.format('%s │ ', label)
+  return string.format('%s │ ', kind)
 end
 
 local function format(entry, item)
   return vim.tbl_extend('force', item, {
     menu = nil,
     abbr = ensure_lengh(item.abbr or '', 50),
-    kind = icon(item, entry),
+    kind = icon(entry, item),
   })
 end
 
