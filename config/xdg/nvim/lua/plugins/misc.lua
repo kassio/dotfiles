1d0bfd4bf01afe08d96f6d6bc1faeef498ad3943
Kassio Borges <kassioborgesm@gmail.com>
2024-01-01 19:32:30 -0300

    nvim: :nail_care:

--- config/xdg/nvim/lua/plugins/misc.lua
+++ config/xdg/nvim/lua/plugins/misc.lua
@@ -1,27 +1,17 @@
 -- plugins without setup/configuration
 return {
-  -- Fix CursorHold
+  -- fix cursorhold
   'antoinemadec/FixCursorHold.nvim',
-  -- Restore last position
+  -- restore last position
   'farmergreg/vim-lastplace',
-  -- Open file with file_path:line:path
+  -- open file with file_path:line:path
   'wsdjeg/vim-fetch',
-  -- Replace variantions of a word
+  -- replace variations of a word
   'tpope/vim-abolish',
-  -- Extended repeat actions with '.'
+  -- extended repeat actions with '.'
   'tpope/vim-repeat',
-  -- Extended text objects
+  -- extended text objects
   'wellle/targets.vim',
-  -- Sudo writer/reader
+  -- sudo writer/reader
   'lambdalisue/suda.vim',
-
-  -- Better language support
-  -- Jsonnet
-  'google/vim-jsonnet',
-  -- Graphql
-  'jparise/vim-graphql',
-  -- Postcss
-  'stephenway/postcss.vim',
-  -- Postcss
-  'tpope/vim-git',
 }
---
+++ config/xdg/nvim/lua/plugins/syntaxes.lua
@@ -0,0 +1,10 @@
+return {
+  -- jsonnet
+  'google/vim-jsonnet',
+  -- graphql
+  'jparise/vim-graphql',
+  -- postcss
+  'stephenway/postcss.vim',
+  -- git, gitcommit, gitconfig, gitrebase, and gitsendemail
+  'tpope/vim-git',
+}
