f452055874291201b193f033106ed2fe8cdb1c21
Kassio Borges <kassioborgesm@gmail.com>
2024-01-04 15:08:18 -0300

    nvim: try to simplify the autoformat

--- config/xdg/nvim/lua/plugins/lsp/autocmds.lua
+++ config/xdg/nvim/lua/plugins/lsp/autocmds.lua
@@ -17,15 +17,5 @@ return {
         require('plugins.lsp.autoformat').setup()
       end,
     })
-
-    vim.api.nvim_create_autocmd({ 'BufEnter' }, {
-      group = aug,
-      callback = function(args)
-        if utils.plugin_filetype(vim.bo[args.buf].filetype) then
-          -- Ensure to not run LSP on plugin buffers
-          return vim.cmd.LspStop()
-        end
-      end,
-    })
   end,
 }
--- config/xdg/nvim/lua/plugins/lsp/autoformat.lua
+++ config/xdg/nvim/lua/plugins/lsp/autoformat.lua
@@ -1,29 +1,28 @@
 local utils = require('utils')
+local function lsp_format(bufnr, async)
+  local filetype = vim.bo[bufnr].filetype
+  if
+    not vim.bo[bufnr].modifiable
+    or filetype == ''
+    or utils.plugin_filetype(filetype)
+    or vim.b[bufnr].skip_autoformat == true
+  then
+    return
+  end
+
+  vim.lsp.buf.format({ async = async })
+end
 
 return {
   setup = function()
-    local function lsp_format(bufnr)
-      local filetype = vim.bo[bufnr].filetype
-      if
-        not vim.bo[bufnr].modifiable
-        or filetype == ''
-        or utils.plugin_filetype(filetype)
-        or vim.b[bufnr].skip_autoformat == true
-      then
-        return
-      end
-
-      vim.lsp.buf.format({ async = false })
-    end
-
     vim.api.nvim_create_user_command('LspFormat', function()
-      lsp_format(vim.api.nvim_get_current_buf())
+      lsp_format(vim.api.nvim_get_current_buf(), true)
     end, {})
 
     vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
       group = vim.api.nvim_create_augroup('user:lsp', { clear = false }),
       callback = function(args)
-        lsp_format(args.buf)
+        lsp_format(args.buf, false)
       end,
     })
   end,
