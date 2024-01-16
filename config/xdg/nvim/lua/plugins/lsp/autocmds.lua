f626125df6760f5054b4a0bf35d3445044398fbc
Kassio Borges <kassioborgesm@gmail.com>
2024-01-16 10:42:50 -0300

    nvim: avoid lsp in plugin buffers

--- config/xdg/nvim/lua/plugins/lsp/autocmds.lua
+++ config/xdg/nvim/lua/plugins/lsp/autocmds.lua
@@ -1,6 +1,19 @@
+local utils = require('utils')
+
 return {
   setup = function()
     local aug = vim.api.nvim_create_augroup('user:lsp', { clear = false })
+
+    vim.api.nvim_create_autocmd({ 'BufEnter', 'BufNewFile' }, {
+      group = aug,
+      callback = function(args)
+        local clients = vim.lsp.get_clients({ bufnr = args.buf })
+
+        if utils.plugin_filetype() then
+          vim.lsp.stop_client(clients)
+        end
+      end,
+    })
 
     vim.api.nvim_create_autocmd({ 'LspAttach' }, {
       group = aug,
