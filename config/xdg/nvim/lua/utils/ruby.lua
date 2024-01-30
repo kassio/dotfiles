a1d4ae1a6e59d637ee051307d4d917c2d06da646
Kassio Borges <kassioborgesm@gmail.com>
2024-01-30 08:04:53 -0300

    nvim: add some _smart_ ruby snippets

--- config/xdg/nvim/lua/utils/ruby.lua
+++ config/xdg/nvim/lua/utils/ruby.lua
@@ -24,4 +24,19 @@ function M.file_namespace()
   return path
 end
 
+function M.rubocop_code()
+  local lnum = vim.fn.getpos('.')[2]
+  local all_diagnostics = vim.diagnostic.get(0, { lnum = lnum })
+
+  if #all_diagnostics == 0 then
+    return ''
+  end
+
+  local diagnostics = vim.tbl_filter(function(diagnostic)
+    return diagnostic.source == 'rubocop'
+  end, all_diagnostics)
+
+  return vim.tbl_get(diagnostics, 1, 'code') or 'CodeReuse/ActiveRecord'
+end
+
 return M
