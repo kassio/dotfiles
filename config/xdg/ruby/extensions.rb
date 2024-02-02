dc2cab3a85797b4fac050cc34e70963eafb93b0c
Kassio Borges <kassioborgesm@gmail.com>
2024-02-02 12:39:10 -0300

    ruby: extract Kassio module to be reused in other places

--- config/xdg/ruby/extensions.rb
+++ config/xdg/ruby/extensions.rb
@@ -1,42 +1,6 @@
 # frozen_string_literal: true.execute
 
-module Kassio
-  extend self
-
-  def load!
-    # Avoid defining global functions to avoid confusion!!
-    # Ideally, define a Kassio::Helper, module with the helper functions
-    load ".console.local.rb" if File.exist?(".console.local.rb")
-  end
-
-  def reload = load!
-
-  def copy(obj)
-    msg = obj.is_a?(String) ? obj : obj.inspect
-
-    `echo "#{msg}" | pbcopy`
-
-    msg
-  end
-
-  def silence_rails_warnings!
-    return unless defined?(Rails)
-
-    ActiveSupport::Deprecation.silenced = true
-  end
-
-  def enable_active_record_log!
-    return unless defined?(Rails)
-
-    ActiveRecord::Base.logger = Logger.new($stdout)
-  end
-
-  def disable_active_record_log!
-    return unless defined?(Rails)
-
-    ActiveRecord::Base.logger = nil
-  end
-end
+load("#{ENV['HOME']}/.dotfiles/config/xdg/ruby/extensions/kassio.rb")
 
 Kassio.silence_rails_warnings!
 Kassio.disable_active_record_log!
---
+++ config/xdg/ruby/extensions/kassio.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true.execute
+
+module Kassio
+  extend self
+
+  def load!
+    # Avoid defining global functions to avoid confusion!!
+    # Ideally, define a Kassio::Helper, module with the helper functions
+    load ".console.local.rb" if File.exist?(".console.local.rb")
+  end
+
+  def reload = load!
+
+  def self.log(*args)
+    File.open('log/kassio.log', 'a') do |f|
+      f << "» " << caller(3, 1)[0] << "\n"
+      args.each { |arg| f << arg.inspect << "\n" }
+      f << "──────────────────────────────────────────────────" << "\n"
+    end
+
+    args
+  end
+
+  def copy(obj)
+    `echo "#{obj.is_a?(String) ? obj : obj.inspect}" | pbcopy`
+
+    msg
+  end
+
+  def silence_rails_warnings!
+    return unless defined?(Rails)
+
+    ActiveSupport::Deprecation.silenced = true
+  end
+
+  def enable_active_record_log!
+    return unless defined?(Rails)
+
+    ActiveRecord::Base.logger = Logger.new($stdout)
+  end
+
+  def disable_active_record_log!
+    return unless defined?(Rails)
+
+    ActiveRecord::Base.logger = nil
+  end
+end
