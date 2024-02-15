03f677b542fa98fc2739dc373c8352005b8281a5
Kassio Borges <kassioborgesm@gmail.com>
2024-02-15 12:41:25 +0000

    nvim: simplify notification appearance

--- config/xdg/nvim/lua/plugins/ui/notification.lua
+++ config/xdg/nvim/lua/plugins/ui/notification.lua
@@ -4,8 +4,17 @@ return {
     local notify = require('notify')
 
     notify.setup({
-      timeout = 1250,
+      fps = 1,
+      timeout = 1500,
       top_down = false,
+      render = function(_bufnr, notification, _highlights, _config)
+        if (notification.title or '') ~= '' then
+          return 'default'
+        else
+          return 'minimal'
+        end
+      end,
+      stages = 'fade',
     })
 
     vim.notify = notify
