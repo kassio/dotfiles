327a49c8e53a9372d89569e83a4af9574bfc386c
Kassio Borges <kassioborgesm@gmail.com>
2024-02-15 10:17:35 +0000

    nvim:notification:remove unused events

--- config/xdg/nvim/lua/plugins/ui/notification.lua
+++ config/xdg/nvim/lua/plugins/ui/notification.lua
@@ -5,12 +5,6 @@ return {
 
     notify.setup({
       timeout = 1250,
-      on_open = function()
-        vim.cmd('doautocmd User NotificationOpened')
-      end,
-      on_close = function()
-        vim.cmd('doautocmd User NotificationClosed')
-      end,
       top_down = false,
     })
 
