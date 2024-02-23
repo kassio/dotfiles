e07b9ad1efdf6eeb35276dea4eabde6ea3eb3df8
Kassio Borges <kassioborgesm@gmail.com>
2024-02-23 18:40:15 +0000

    nvim: make ruby lsp custom commands easier to extend

--- config/xdg/nvim/lua/plugins/lsp/servers/ruby_ls/commands.lua
+++ config/xdg/nvim/lua/plugins/lsp/servers/ruby_ls/commands.lua
@@ -1,23 +1,27 @@
-local INCLUDE_INDIRECT_OPTION = 'include_indirect'
+local function request(bufnr, method, cb)
+  local client = vim.lsp.get_clients({ name = 'ruby_ls' })[1]
+  local params = vim.lsp.util.make_text_document_params(bufnr)
+
+  client.request(method, params, function(error, response)
+    if error ~= nil then
+      vim.notify(
+        string.format('%s\n%s', error.message, error.data.backtrace),
+        vim.log.levels.ERROR,
+        { title = 'Ruby LSP' }
+      )
+    end
+
+    cb(response, error, client)
+  end, bufnr)
+end
 
 return {
   setup = function(bufnr)
+    local INCLUDE_INDIRECT_OPTION = 'include_indirect'
     vim.api.nvim_buf_create_user_command(bufnr, 'LspRubyShowDependencies', function(opts)
-      local method = 'rubyLsp/workspace/dependencies'
-      local client = vim.lsp.get_clients({ name = 'ruby_ls' })[1]
-      local include_indirect = opts.args == INCLUDE_INDIRECT_OPTION
-
-      local params = vim.lsp.util.make_text_document_params(bufnr)
-      client.request(method, params, function(error, dependencies_list)
-        if error ~= nil then
-          vim.notify(
-            'Error loading dependencies: ' .. error,
-            vim.log.levels.ERROR,
-            { title = 'Ruby LSP' }
-          )
-        end
-
-        local dependencies = vim.fn.reduce(dependencies_list, function(result, item)
+      request(bufnr, 'rubyLsp/workspace/dependencies', function(response)
+        local include_indirect = opts.args == INCLUDE_INDIRECT_OPTION
+        local dependencies = vim.fn.reduce(response, function(result, item)
           local label = string.format('%s (%s)', item.name, item.version.version)
           if not include_indirect and not item.dependency then
             return result
@@ -34,7 +38,7 @@ return {
 
         vim.fn.setqflist(dependencies)
         vim.cmd.copen()
-      end, bufnr)
+      end)
     end, {
       nargs = '?',
       complete = function()
