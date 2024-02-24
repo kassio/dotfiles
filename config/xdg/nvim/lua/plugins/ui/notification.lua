540e4f12eac36e671ebd0afd0a6bc9a3c7a7eb11
Kassio Borges <kassioborgesm@gmail.com>
2024-02-24 13:57:43 +0000

    nvim: remove notifications animations

--- config/xdg/nvim/lua/plugins/ui/notification.lua
+++ config/xdg/nvim/lua/plugins/ui/notification.lua
@@ -4,17 +4,9 @@ return {
     local notify = require('notify')
 
     notify.setup({
-      fps = 1,
       timeout = 1500,
       top_down = false,
-      render = function(_bufnr, notification, _highlights, _config)
-        if (notification.title or '') ~= '' then
-          return 'default'
-        else
-          return 'minimal'
-        end
-      end,
-      stages = 'fade',
+      stages = 'no_animation'
     })
 
     vim.notify = notify
