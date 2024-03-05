edbc0391be94b52c95bfca9e270d1eb4a697d54d
Kassio Borges <kassioborgesm@gmail.com>
2024-03-05 19:06:05 +0000

    nvim: avoid colorizing by name

--- config/xdg/nvim/lua/plugins/ui/colors.lua
+++ config/xdg/nvim/lua/plugins/ui/colors.lua
@@ -2,7 +2,16 @@ return {
   -- Highlight color strings
   {
     'NvChad/nvim-colorizer.lua',
-    config = true,
+    opts = {
+      user_default_options = {
+        RGB = true,
+        RRGGBB = true,
+        names = false,
+        RRGGBBAA = true,
+        mode = 'background',
+        always_update = false,
+      },
+    },
   },
 
   -- Fix terminal colors
