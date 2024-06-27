77447fa6e4ba849ed9e11e5e1613492a8819c951
Kassio Borges <kassioborgesm@gmail.com>
2024-06-27 21:22:34 +0100

    wezterm: add more search keymaps

--- config/xdg/wezterm/key_tables.lua
+++ config/xdg/wezterm/key_tables.lua
@@ -3,7 +3,23 @@ return {
   setup = function(wezterm)
     return {
       search_mode = {
-        { key = 'Enter', mods = 'NONE', action = wezterm.action.CopyMode('PriorMatch') },
+        {
+          key = 'Enter',
+          mods = 'NONE',
+          action = wezterm.action.Multiple({
+            wezterm.action.CopyTo('ClipboardAndPrimarySelection'),
+            wezterm.action.CopyMode('Close'),
+          }),
+        },
+        {
+          key = 'Enter',
+          mods = 'CMD',
+          action = wezterm.action.Multiple({
+            wezterm.action.CopyTo('ClipboardAndPrimarySelection'),
+            wezterm.action.CopyMode('Close'),
+            wezterm.action.PasteFrom('Clipboard'),
+          }),
+        },
         { key = 'Escape', mods = 'NONE', action = wezterm.action.CopyMode('Close') },
         { key = 'f', mods = 'CTRL', action = wezterm.action.CopyMode('CycleMatchType') },
         { key = 'n', mods = 'CTRL', action = wezterm.action.CopyMode('NextMatch') },
