6f53f09d3992f6dd9d0671fb109690abd23493ae
Kassio Borges <kassioborgesm@gmail.com>
2024-07-05 20:26:20 +0100

    nvim: fix :xa when terminal is open

--- config/xdg/nvim/lua/config/terminal/autocmds.lua
+++ config/xdg/nvim/lua/config/terminal/autocmds.lua
@@ -2,6 +2,17 @@ return {
   setup = function(manager)
     local group = vim.api.nvim_create_augroup('user:terminal', { clear = false })
 
+    vim.api.nvim_create_autocmd({ 'ExitPre' }, {
+      group = group,
+      callback = function()
+        for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+          if vim.bo[bufnr].buftype == 'terminal' then
+            vim.api.nvim_buf_delete(bufnr, { force = true })
+          end
+        end
+      end,
+    })
+
     vim.api.nvim_create_autocmd({ 'TermOpen' }, {
       group = group,
       callback = function(event)
