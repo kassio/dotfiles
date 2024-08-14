ca7781d038b683eb0dd1a5b03afa7c88b1bd9128
Kassio Borges <kassioborgesm@gmail.com>
2024-08-14 14:29:08 +0100

    nvim:add more yaml schemas

--- config/xdg/nvim/lua/plugins/lsp/servers/yamlls.lua
+++ config/xdg/nvim/lua/plugins/lsp/servers/yamlls.lua
@@ -1,11 +1,21 @@
-local gitlab_schema =
-  'https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json'
+local schemas = {
+  gitlab = {
+    ci = 'https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json',
+    reporter = 'https://gitlab.com/gitlab-newscast/reporter/-/raw/main/reporter/lib/config/schema.json',
+  },
+}
 
 return {
   settings = {
     yaml = {
       keyOrdering = false,
+      schemas = {
+        [schemas.gitlab.ci] = {
+          'gitlab-ci.yml',
+          '.gitlab/*.yml',
+        },
+        [schemas.gitlab.reporter] = 'reports.*.yml',
+      },
     },
-    [gitlab_schema] = '/.gitlab-ci.yml',
   },
 }
