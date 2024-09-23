2f13bd22c6e2b8d082bc75d47b606fff93c11363
Kassio Borges <kassioborgesm@gmail.com>
2024-09-23 10:02:29 +0200

    nvim:ruby: fix namespace fetcher to not sufix with ::

--- config/xdg/nvim/lua/plugins/treesitter/fetcher/ruby.lua
+++ config/xdg/nvim/lua/plugins/treesitter/fetcher/ruby.lua
@@ -15,9 +15,19 @@ return {
     local type = node:type()
 
     local text
-    if vim.tbl_contains({ 'assignment', 'class', 'module' }, type) then
+    if
+      (
+        vim.tbl_contains({ 'module', 'class', 'singleton_class' }, get_parent_type(node, 2))
+        or get_parent_type(node, 1) == 'program'
+      ) and vim.tbl_contains({ 'assignment' }, type)
+    then
+      -- constant assignment
+      text = tsutils.next_children_text(node, '::', { 'constant' })
+    elseif vim.tbl_contains({ 'class', 'module' }, type) then
+      -- constant nesting
       text = tsutils.next_children_text(node, '::', { 'constant' })
     elseif vim.tbl_contains({ 'method', 'singleton_method' }, type) then
+      -- singleton method
       text = tsutils.next_children_text(node, '::', { 'identifier' })
     end
 
