5089107ae927a74e3533fc193d69f07213e6e69d
Kassio Borges <kassioborgesm@gmail.com>
2024-09-26 15:01:35 +0100

    nvim:database:more UI configs

--- config/xdg/nvim/lua/plugins/database.lua
+++ config/xdg/nvim/lua/plugins/database.lua
@@ -15,6 +15,9 @@ return {
   },
   init = function()
     vim.g.db_ui_use_nerd_fonts = 1
+    vim.g.db_ui_use_nvim_notify = 1
+    vim.g.db_ui_show_database_icon = 1
+    vim.g.db_ui_win_position = 'right'
     vim.g.db_ui_save_location = '~/.local/share/db_ui'
     vim.g.db_ui_tmp_query_location = '~/.local/share/db_ui'
   end,
