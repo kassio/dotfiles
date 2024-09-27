7744624a153b13825bfaa7677ce97f692df43793
Kassio Borges <kassioborgesm@gmail.com>
2024-09-27 10:05:47 +0100

    nvim:database: simplify query backup

--- config/xdg/nvim/lua/plugins/database.lua
+++ config/xdg/nvim/lua/plugins/database.lua
@@ -18,7 +18,7 @@ return {
     vim.g.db_ui_use_nvim_notify = 1
     vim.g.db_ui_show_database_icon = 1
     vim.g.db_ui_win_position = 'right'
-    vim.g.db_ui_save_location = '~/.local/share/db_ui'
-    vim.g.db_ui_tmp_query_location = '~/.local/share/db_ui'
+    vim.g.db_ui_save_location = '~/src/sync/database'
+    vim.g.db_ui_tmp_query_location = '~/src/sync/database'
   end,
 }
