2d7fa30b8066a3a07d6f4f75cad93201eda08c33
Kassio Borges <kassioborgesm@gmail.com>
2024-10-16 13:50:39 +0200

    nvim:ruby: make lsp config more explicit

--- config/xdg/nvim/lua/plugins/lsp/servers/ruby_lsp/init.lua
+++ config/xdg/nvim/lua/plugins/lsp/servers/ruby_lsp/init.lua
@@ -2,7 +2,41 @@ return {
   cmd = { 'bundle', 'exec', 'ruby-lsp' },
   settings = {
     init_options = {
-      enableExperimentalFeatures = true,
+      enabled_features = {
+        code_actions = true,
+        code_lens = true,
+        completion = true,
+        definition = true,
+        diagnostics = true,
+        document_highlights = true,
+        document_link = true,
+        document_symbols = true,
+        folding_ranges = true,
+        formatting = true,
+        hover = true,
+        inlay_hint = true,
+        on_type_formatting = true,
+        selection_ranges = true,
+        semantic_highlighting = true,
+        signature_help = true,
+        type_hierarchy = true,
+        workspace_symbol = true,
+      },
+      features_configuration = {
+        inlay_hint = {
+          implicit_hash_value = true,
+          implicit_rescue = true,
+        },
+      },
+      -- indexing = {
+      --   excluded_patterns = { 'path/to/excluded/file.rb' },
+      --   included_patterns = { 'path/to/included/file.rb' },
+      --   excluded_gems = { 'gem1', 'gem2', 'etc.' },
+      --   excluded_magic_comments = { 'compiled:true' },
+      -- },
+      formatter = 'auto',
+      linters = {},
+      experimental_features_enabled = true,
     },
   },
 }
