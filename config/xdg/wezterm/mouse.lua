eeb700aee5f248225b9612fb3ce379f462848ef1
Kassio Borges <kassioborgesm@gmail.com>
2025-02-01 19:17:50 +0100

    wezterm: copy on selection/paste with ctrl+click

--- config/xdg/wezterm/mouse.lua
+++ config/xdg/wezterm/mouse.lua
@@ -2,21 +2,21 @@ return {
   setup = function(wezterm)
     return {
       { -- disable click
-        event = {
-          Up = {
-            streak = 1,
-            button = 'Left',
-          },
-        },
+        event = { Up = { streak = 1, button = 'Left' } },
         action = wezterm.action.Nop,
       },
+      { -- always copy selection
+        event = { Up = { streak = 1, button = 'Left' } },
+        mods = 'NONE',
+        action = wezterm.action({ CompleteSelectionOrOpenLinkAtMouseCursor = 'Clipboard' }),
+      },
+      { -- paste on ctrl+click
+        event = { Up = { streak = 1, button = 'Left' } },
+        mods = 'CTRL',
+        action = wezterm.action.PasteFrom('Clipboard'),
+      },
       { -- quicklook selection
-        event = {
-          Up = {
-            streak = 1,
-            button = 'Left',
-          },
-        },
+        event = { Up = { streak = 1, button = 'Left' } },
         mods = 'CMD|SHIFT',
         action = wezterm.action_callback(function(window, pane)
           local selection = window:get_selection_text_for_pane(pane)
@@ -26,12 +26,7 @@ return {
         end),
       },
       { -- open selection/link
-        event = {
-          Up = {
-            streak = 1,
-            button = 'Left',
-          },
-        },
+        event = { Up = { streak = 1, button = 'Left' } },
         mods = 'CMD',
         action = wezterm.action_callback(function(window, pane)
           local selection = window:get_selection_text_for_pane(pane)
