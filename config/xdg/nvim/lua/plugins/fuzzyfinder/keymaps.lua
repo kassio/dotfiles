9ec3cd7ef1a5e1785be8ae89949aa362c3957fd7
Kassio Borges <kassioborgesm@gmail.com>
2025-10-09 13:22:53 +0200

    nvim: add keymaps to search files based on current word

--- config/xdg/nvim/lua/plugins/fuzzyfinder/keymaps.lua
+++ config/xdg/nvim/lua/plugins/fuzzyfinder/keymaps.lua
@@ -1,8 +1,26 @@
 return {
   setup = function(ffinder)
     -- files
-    vim.keymap.set('n', '<leader>ff', ffinder.global, { desc = 'find:global' })
     vim.keymap.set('n', '<leader>fo', ffinder.oldfiles, { desc = 'find:oldfiles' })
+    vim.keymap.set('n', '<leader>ff', ffinder.global, { desc = 'find:global' })
+    vim.keymap.set('n', '<leader>fF', function ()
+      local word = require('utils').cword()
+
+      ffinder.files({
+        query = word,
+        prompt = string.format('files:%s', word),
+      })
+    end, { desc = 'find:current word(underscore)' })
+    vim.keymap.set('n', '<leader>f_', function ()
+      local word = require('utils').cword()
+      word, _ = require('utils.string').snakecase(word):gsub('::', '/')
+
+      ffinder.files({
+        query = word,
+        prompt = string.format('files:%s', word),
+      })
+    end, { desc = 'find:current word(underscore)' })
+
 
     -- buffer
     vim.keymap.set('n', '<leader>fn', ffinder.blines, { desc = 'find:current buffer lines' })
