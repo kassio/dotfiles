caeab8268419d93d536cebcbe4953d7cfadee86e
Kassio Borges <kassioborgesm@gmail.com>
2025-10-13 11:23:28 +0200

    nvim:fix git fuzzy finder to only show left files and not display time messages

--- config/xdg/nvim/lua/plugins/fuzzyfinder/keymaps/git.lua
+++ config/xdg/nvim/lua/plugins/fuzzyfinder/keymaps/git.lua
@@ -2,12 +2,13 @@ return {
   setup = function(ffinder)
     vim.keymap.set('n', '<leader>fg', function()
       local main = vim.system({ 'git-branch-main' }):wait().stdout or ''
-      local git_diff = [[git --no-pager diff --diff-filter=AMRUX --name-only --relative %s]]
+      local git_diff = [[git --no-pager diff --diff-filter=AMRUX --name-only --relative %s...]]
       local modified = string.format(git_diff, 'HEAD')
       local changed = string.format(git_diff, vim.trim(main))
       local untracked = [[git --no-pager ls-files --others --exclude-standard]]
 
       ffinder.git_files({
+        silent = true,
         cwd = vim.uv.cwd(),
         prompt = "git:diff> ",
         cmd = string.format("{ %s; %s; %s; } | sort -u", modified, changed, untracked),
