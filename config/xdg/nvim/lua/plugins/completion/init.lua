3e3ebf1f0269e0a0bd358088bfada269c8d9320f
Kassio Borges <kassioborgesm@gmail.com>
2025-10-15 12:20:19 +0200

    nvim:more completion configs

--- config/xdg/nvim/lua/plugins/completion/init.lua
+++ config/xdg/nvim/lua/plugins/completion/init.lua
@@ -3,15 +3,50 @@ return {
   dependencies = { 'rafamadriz/friendly-snippets' },
   version = '1.*',
   opts = {
-    keymap = { preset = 'default' },
     appearance = {
       nerd_font_variant = 'mono'
     },
-    completion = { documentation = { auto_show = false } },
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
+    fuzzy = {
+      implementation = "prefer_rust_with_warning",
     },
-    fuzzy = { implementation = "prefer_rust_with_warning" }
+    completion = {
+      documentation = {
+        auto_show = true,
+      },
+    },
+    keymap = {
+      preset = 'default',
+
+      ['<C-n>'] = {
+        'show',
+        'select_next'
+      },
+      ['<C-k>'] = {
+        'show_signature',
+        'hide_signature',
+        'fallback',
+      },
+    },
+    sources = {
+      default = {
+        'snippets',
+        'lsp',
+        'path',
+        'buffer',
+      },
+      min_keyword_length = 0,
+      providers = {
+        path = {
+          opts = {
+            get_cwd = function(_)
+              return vim.fn.getcwd()
+            end,
+          },
+        },
+      },
+    },
   },
-  opts_extend = { "sources.default" }
+  opts_extend = {
+    "sources.default",
+  }
 }
