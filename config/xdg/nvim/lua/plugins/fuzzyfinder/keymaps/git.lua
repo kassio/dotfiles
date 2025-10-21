dd88754c1d5e3612a166a9267b4b59351b6eccac
Kassio Borges <kassioborgesm@gmail.com>
2025-10-21 12:10:17 +0200

    nvim:fuzzy finder: fix git finder

--- config/xdg/nvim/lua/plugins/fuzzyfinder/keymaps/git.lua
+++ config/xdg/nvim/lua/plugins/fuzzyfinder/keymaps/git.lua
@@ -2,16 +2,17 @@ return {
   setup = function(ffinder)
     vim.keymap.set('n', '<leader>fg', function()
       local main = vim.system({ 'git-branch-main' }):wait().stdout or ''
-      local git_diff = [[git --no-pager diff --diff-filter=AMRUX --name-only --relative %s...]]
+      local git_diff = [[git --no-pager diff --diff-filter=AMRUX --name-only --relative %s]]
       local modified = string.format(git_diff, 'HEAD')
-      local changed = string.format(git_diff, vim.trim(main))
+      local changed = string.format(git_diff, vim.trim(main) .. '...')
       local untracked = [[git --no-pager ls-files --others --exclude-standard]]
+      local command = string.format("{ %s; %s; %s; } | sort -u", modified, changed, untracked)
 
       ffinder.git_files({
         silent = true,
         cwd = vim.uv.cwd(),
         prompt = "git:diff> ",
-        cmd = string.format("{ %s; %s; %s; } | sort -u", modified, changed, untracked),
+        cmd = command,
       })
     end, { desc = 'find:git:diff' })
   end
