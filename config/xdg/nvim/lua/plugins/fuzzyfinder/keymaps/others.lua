7bdda73f77e513f725504c013d80d3f81c00bd2f
Kassio Borges <kassioborgesm@gmail.com>
2025-10-22 11:41:09 +0200

    nvim: fa for all fuzzy finder commands

--- config/xdg/nvim/lua/plugins/fuzzyfinder/keymaps/others.lua
+++ config/xdg/nvim/lua/plugins/fuzzyfinder/keymaps/others.lua
@@ -1,5 +1,6 @@
 return {
   setup = function(ffinder)
+    vim.keymap.set('n', '<leader>fa', ffinder.builtin, { desc = 'find:finders' })
     vim.keymap.set('n', '<leader>fh', ffinder.helptags, { desc = 'find:help' })
     vim.keymap.set({ 'n', 'x' }, '<leader>fH', function()
       local word = require('utils').cword()
