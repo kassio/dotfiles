7b114ebcfdb6c9774a36475590f35befadf72920
Kassio Borges <kassioborgesm@gmail.com>
2025-10-22 10:57:13 +0200

    nvim:add fH for help for the current word

--- config/xdg/nvim/lua/plugins/fuzzyfinder/keymaps/file.lua
+++ config/xdg/nvim/lua/plugins/fuzzyfinder/keymaps/file.lua
@@ -2,22 +2,16 @@ return {
   setup = function(ffinder)
     vim.keymap.set('n', '<leader>fo', ffinder.oldfiles, { desc = 'find:oldfiles' })
     vim.keymap.set('n', '<leader>ff', ffinder.global, { desc = 'find:global' })
+
     vim.keymap.set({ 'n', 'x' }, '<leader>fF', function ()
       local word = require('utils').cword()
-
-      ffinder.files({
-        query = word,
-        prompt = string.format('files:%s', word),
-      })
+      ffinder.files({ query = word })
     end, { desc = 'find:current word(underscore)' })
+
     vim.keymap.set({ 'n', 'x' }, '<leader>f_', function ()
       local word = require('utils').cword()
       word, _ = require('utils.string').snakecase(word):gsub('::', '/')
-
-      ffinder.files({
-        query = word,
-        prompt = string.format('files:%s', word),
-      })
+      ffinder.files({ query = word })
     end, { desc = 'find:current word(underscore)' })
   end
 }
--- config/xdg/nvim/lua/plugins/fuzzyfinder/keymaps/others.lua
+++ config/xdg/nvim/lua/plugins/fuzzyfinder/keymaps/others.lua
@@ -1,6 +1,12 @@
 return {
   setup = function(ffinder)
     vim.keymap.set('n', '<leader>fh', ffinder.helptags, { desc = 'find:help' })
+    vim.keymap.set({ 'n', 'x' }, '<leader>fH', function()
+      local word = require('utils').cword()
+
+      ffinder.helptags({ query = word })
+    end, { desc = 'find:help' })
+
     vim.keymap.set('n', '<leader>fk', ffinder.keymaps, { desc = 'find:keymaps' })
     vim.keymap.set('n', '<leader>fp', ffinder.resume, { desc = 'find:resume' })
   end
