84dbf2bbb703623d784673eb163b5f097b2c8180
Kassio Borges <kassioborgesm@gmail.com>
2025-12-15 09:56:28 +0100

    nvim:git:add GitDiffClose to close diff windows on current tab

--- config/xdg/nvim/lua/plugins/git/commands.lua
+++ config/xdg/nvim/lua/plugins/git/commands.lua
@@ -13,14 +13,33 @@ end
 return {
   setup = function()
     command('BrowseRepository', git.open_repository_url, { desc = 'open origin in the browser' })
-    command('Blame', git.blame, { desc = 'blame file' })
     command('OpenNewFiles', git.open_new_files, { desc = 'open unstaged files' })
+
+    command('Blame', git.blame, { desc = 'blame file' })
+    vim.cmd.cabbrev('Gb GitBlame')
+
     command('ResetBuffer', git.reset_buffer, { desc = 'restore current file' })
+    vim.cmd.cabbrev('Grt GitResetBuffer')
+
     command('Write', git.stage_buffer, { desc = 'restore current file' })
+    vim.cmd.cabbrev('Gw GitWrite')
 
     command('Diff', function(cmd)
       git.diff_this(cmd.args)
     end, { nargs = '?', desc = 'diff this file with given ref or main' })
+    vim.cmd.cabbrev('Gd GitDiff')
+
+    command('DiffClose', function ()
+      for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+        local buf = vim.api.nvim_win_get_buf(win)
+        local name = vim.api.nvim_buf_get_name(buf)
+
+        if name:match("^gitsigns://") then
+          vim.api.nvim_win_close(win, true)
+        end
+      end
+    end, { desc = 'close diff windows on current tab' })
+    vim.cmd.cabbrev('Gdc GitDiffClose')
 
     command('BrowseMergeRequest', function()
       vim.fn.jobstart({ 'git-browse-merge-request' })
@@ -34,9 +53,5 @@ return {
       git.copy_remote_url(cmd.args)
     end, { nargs = '?', bang = true, desc = 'copy remote file url' })
 
-    vim.cmd.cabbrev('Gb GitBlame')
-    vim.cmd.cabbrev('Gd GitDiff')
-    vim.cmd.cabbrev('Grt GitResetBuffer')
-    vim.cmd.cabbrev('Gw GitWrite')
   end,
 }
