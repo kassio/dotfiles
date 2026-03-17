735fcc0d49bccd2fb4604ec5b6d475e1168a8ac0
Kassio Borges <kassioborgesm@gmail.com>
2026-03-17 15:57:24 +0100

    nvim:remove old require

--- config/xdg/nvim/lua/plugins/completion/snippets/init.lua
+++ config/xdg/nvim/lua/plugins/completion/snippets/init.lua
@@ -23,7 +23,6 @@ return {
     require('plugins.completion.snippets.ruby').setup()
     require('plugins.completion.snippets.rails').setup()
     require('plugins.completion.snippets.rspec').setup()
-    require('plugins.completion.snippets.gitlab').setup()
     -- elixir
     require('plugins.completion.snippets.elixir').setup()
   end,
